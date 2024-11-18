fb5d4680906298e77cc47c8d69cf8ae6e701145d
Ronnie Kakunguwo <ronniekakunguwo03@gmail.com>
2024-11-18 15:04:03 +0200

    WEEK 1

---
+++ Week 1/user_input.py
@@ -0,0 +1,7 @@
+name = input("What is your name?\n...")
+
+age = input("How old are you?\n...")
+
+location = input("Where do you live?\n...")
+
+print(f"Hello {name}, you are {age} years old and you live in {location}")
